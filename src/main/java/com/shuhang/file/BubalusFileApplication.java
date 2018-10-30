fb58ed170a1ea359e04ba978f64bd44db751f69e
张明飞 <mingfei.zhang@chinaredstar.com>
2018-10-30 09:32:05 +0800

    初始提交

---
+++ src/main/java/com/shuhang/file/BubalusFileApplication.java
@@ -0,0 +1,12 @@
+package com.shuhang.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubalusFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubalusFileApplication.class, args);
+	}
+}
